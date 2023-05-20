41df1a9905c8fedf0089cd6ac476690bca43db3a
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-05-20 01:15:36 -0300

    tratativa de erros

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+import fs from 'fs';
 import chalk from 'chalk'; //es6 preferir assim que require(x)
 
-console.log(chalk.blue('olá mundo'));
+function handleError(error) {
+    throw new Error(chalk.red('Falha ao ler arquivo: ', error.code));
+}
+
+function getFile(filePath) {
+    const encoding = 'utf-8';
+
+    return fs.readFile(filePath, encoding, (error, text) => {
+        if (error) handleError(error);
+
+        console.log(chalk.green(text));
+    });
+}
+
+getFile('./arquivos/texto.md');
