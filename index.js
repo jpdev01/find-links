27847a9cb28a1bbb3e7bec307fb36fce02fec9a3
Joao Pedro Truchinski Borba <joao.borba@asaas.com.br>
2023-05-21 13:51:25 -0300

    upd: usando async await

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ function handleError(error) {
     throw new Error(chalk.red('Falha ao ler arquivo: ', error.code));
 }
 
-function getFile(filePath) {
-    const encoding = 'utf-8';
-
-    return fs.readFile(filePath, encoding, (error, text) => {
-        if (error) handleError(error);
-
+async function getFile(filePath) {
+    try {
+        const encoding = 'utf-8';
+        const text = await fs.promises.readFile(filePath, encoding)
         console.log(chalk.green(text));
-    });
+    } catch (error) {
+        handleError(error);
+    }
+
+    // fs.promises.readFile(filePath, encoding)
+    //     .then(text => console.log(chalk.green(text)))
+    //     .catch(handleError);
 }
 
 getFile('./arquivos/texto.md');
